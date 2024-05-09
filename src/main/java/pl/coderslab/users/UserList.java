e1c64af25f2caa612d64b171340a77ebbe4ad373
AnnLiz22 <bielakam@gmail.com>
2024-05-09 10:56:01 +0200

    main page view - list of users

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -1,16 +1,27 @@
 package pl.coderslab.users;
 
+import pl.coderslab.UserDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/user/list")
 public class UserList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
+
+        UserDao userDao = new UserDao();
+        try {
+            request.setAttribute("users", userDao.readAllUsers());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,4 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: chrislietz
-  Date: 06/05/2024
-  Time: 23:29
-  To change this template use File | Settings | File Templates.
---%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
