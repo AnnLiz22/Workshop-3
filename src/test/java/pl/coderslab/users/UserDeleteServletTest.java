3adbded38697463bebfbc22c82669eb47c4c9417
AnnLiz22 <bielakam@gmail.com>
2024-09-15 22:30:26 +0200

    added class for testing user delete servlet

---
+++ src/test/java/pl/coderslab/users/UserDeleteServletTest.java
@@ -0,0 +1,12 @@
+package pl.coderslab.users;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserDeleteServletTest {
+
+  @Test
+  void doGet() {
+  }
+}
