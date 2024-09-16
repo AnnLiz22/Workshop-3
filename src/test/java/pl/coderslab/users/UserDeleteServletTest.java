671b9e60d4faf4592e5cd995531f8fa35ef69055
AnnLiz22 <bielakam@gmail.com>
2024-09-16 23:41:27 +0200

    added beforeEach set up method

--- src/test/java/pl/coderslab/users/UserDeleteServletTest.java
+++ src/test/java/pl/coderslab/users/UserDeleteServletTest.java
@@ -2,9 +2,33 @@ package pl.coderslab.users;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.coderslab.UserDao;
 
 class UserDeleteServletTest {
+
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  private RequestDispatcher requestDispatcher;
+  private UserDao userDao;
+  private UserDeleteServlet userDeleteServlet;
+
+  @BeforeEach
+  void setUp() {
+    request = Mockito.mock(HttpServletRequest.class);
+    response = Mockito.mock(HttpServletResponse.class);
+    requestDispatcher = Mockito.mock(RequestDispatcher.class);
+    userDao = Mockito.mock(UserDao.class);
+
+    userDeleteServlet = new UserDeleteServlet(userDao);
+
+  }
+
 
   @Test
   void doGet() {
